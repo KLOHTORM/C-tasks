9a03df13e454857976e60b1d35d9e6a926670abd
KLOHTORM <klohtorm@gmail.com>
2023-10-19 13:06:17 +0300

    Добавлен 15 таск

---
+++ task15/Program.cs
@@ -0,0 +1,54 @@
+﻿/*
+Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет
+*/
+
+System.Console.WriteLine("Введите число: ");
+int day = Convert.ToInt32(Console.ReadLine());
+
+if (day == 1)
+{
+    System.Console.WriteLine("нет");
+}
+else
+    if (day == 2)
+    {
+        System.Console.WriteLine("нет");
+    }
+    else
+        if (day == 3)
+        {
+            System.Console.WriteLine("нет");
+        }
+        else
+            if (day == 4)
+           {
+                System.Console.WriteLine("нет");
+            }
+            else
+                if (day == 5)
+                {
+                    System.Console.WriteLine("почти");
+                }
+                else
+                    if (day == 6)
+                    {
+                        System.Console.WriteLine("ДА");
+                    }
+                    else
+                        if (day == 7)
+                        {
+                            System.Console.WriteLine("ДА");
+                        }
+                        else
+                            if (day > 7)
+                            {
+                                System.Console.WriteLine("Нет такого дня недели");
+                            }
+                            else
+                                if (day == 0)
+                                {
+                                    System.Console.WriteLine("И какой же по твоему нулевой день недели?");
+                                }
