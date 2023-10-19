59412293926b99e6b500ab051484c843c1785b1e
KLOHTORM <klohtorm@gmail.com>
2023-10-19 13:04:56 +0300

    Добавлен 07 таск

---
+++ task07/Program.cs
@@ -0,0 +1,10 @@
+﻿/*
+Напишите программу, которая на вход принимает трехзначное число и на выходе показывает последнюю цифру этого числа
+*/
+
+System.Console.WriteLine("Введите число: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+int third = userNumber % 10;
+
+System.Console.WriteLine(third);
