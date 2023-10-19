ea828563f3fc936dfb79dd3e27217627dd8e8112
KLOHTORM <klohtorm@gmail.com>
2023-10-19 13:04:45 +0300

    Добавлен 06 таск

---
+++ task06/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+Внутри класса Answer напишите метод CheckIfEven, который принимает число number и выводит, является ли число четным (делится ли оно на 2)
+
+number = 4 -> число 4 четное
+*/
+
+System.Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    System.Console.WriteLine($"Число {number} четное");
+}
+else
+{
+    System.Console.WriteLine($"Число {number} нечетное");
+}
