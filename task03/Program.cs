4f1303d81e2cc68fc5fdd2b78d151de7f14ab400
KLOHTORM <klohtorm@gmail.com>
2023-10-19 13:04:13 +0300

    Добавлен 03 таск

---
+++ task03/Program.cs
@@ -0,0 +1,51 @@
+﻿/*
+Написать пргграмму, определяющую день недели по заданному числу
+*/
+
+System.Console.WriteLine("Введите число: ");
+int day = Convert.ToInt32(Console.ReadLine());
+
+if (day == 1)
+{
+    System.Console.WriteLine("Понедельник");
+}
+else
+    if (day == 2)
+    {
+        System.Console.WriteLine("Вторник");
+    }
+    else
+        if (day == 3)
+        {
+            System.Console.WriteLine("Среда");
+        }
+        else
+            if (day == 4)
+           {
+                System.Console.WriteLine("Четверг");
+            }
+            else
+                if (day == 5)
+                {
+                    System.Console.WriteLine("Пятница");
+                }
+                else
+                    if (day == 6)
+                    {
+                        System.Console.WriteLine("Суббота");
+                    }
+                    else
+                        if (day == 7)
+                        {
+                            System.Console.WriteLine("Воскресенье");
+                        }
+                        else
+                            if (day > 7)
+                            {
+                                System.Console.WriteLine("Нет такого дня недели");
+                            }
+                            else
+                                if (day == 0)
+                                {
+                                    System.Console.WriteLine("И какой же по твоему нулевой день недели?");
+                                }
