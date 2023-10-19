9e44bf5e2b274296979397530b95106178accb50
KLOHTORM <klohtorm@gmail.com>
2023-10-19 13:03:40 +0300

    Добавлен 01 таск

---
+++ task01/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+1. Напишите программу, которая на вход принимает 2 числа и проверяет, является ли 1 число квадратом 2
+*/
+
+System.Console.WriteLine("Введите 1 число: ");
+int userNumber1 = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите 2 число: ");
+int userNumber2 = Convert.ToInt32(Console.ReadLine());
+
+int sqNumber1 = userNumber2 * userNumber2;
+
+bool result = userNumber1 == sqNumber1;
+
+System.Console.WriteLine(result);
