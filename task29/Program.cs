11c861f4c0b52233e01a8910795bfec3bf3b89f5
KLOHTORM <klohtorm@gmail.com>
2023-10-28 17:31:35 +0300

    Добавили таск 29

---
+++ task29/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
+*/
+
+int count = 0;
+
+while (count < 8)
+{
+    int num = new Random().Next(1, 100);
+    System.Console.Write(num);
+    System.Console.Write(", ");
+    count++;
+}
