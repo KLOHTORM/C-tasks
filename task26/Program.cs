961124a3f59fd56e9c6469b1dc8c7bf82d81e5a7
KLOHTORM <klohtorm@gmail.com>
2023-11-12 23:51:44 +0300

    Добавили таск 26

---
+++ task26/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+Напишите программу, которая принимает на вход число 
+и выдаёт количество цифр в числе.
+
+456 -> 3
+78 -> 2
+89126 -> 5
+*/
+
+System.Console.WriteLine("Введите число: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+int count = 0;
+
+int Count(int userNumber)
+{
+    for (int i = userNumber; i > 0; i /= 10)
+    {
+        count = count + 1;
+    }
+    return count;
+}
+
+int userCount = Count(userNumber);
+
+System.Console.WriteLine(userCount);
