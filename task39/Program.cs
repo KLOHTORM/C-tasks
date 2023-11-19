b1cf362d8e6c70204e3e920d22a7e383f23ee983
KLOHTORM <klohtorm@gmail.com>
2023-11-19 23:00:38 +0300

    Добавили таск 39

---
+++ task39/Program.cs
@@ -0,0 +1,61 @@
+﻿/*
+39. Напишите программу, которая перевернёт одномерный массив (последний элемент будет на первом месте, а первый - на последнем и т.д.)
+
+[1 2 3 4 5] -> [5 4 3 2 1]
+[6 7 3 6] -> [6 3 7 6]
+Комментарий: эту задачу можно решить 2 способами: 
+1) менять числа местами в исходном массиве; 
+2) создать новый массив и в него записать перевёрнутый исходный массив по элементам.
+*/
+
+int[] GetRandomArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(1, 11);
+    }
+    return array;
+}
+
+int Prompt(string massage)
+{
+    System.Console.WriteLine(massage);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write(array[i]);
+        if (i < array.Length)
+        {
+            System.Console.Write("  ");
+        }
+    }
+}
+
+void ReverseArray(int[] arr)
+{
+    int temp = 0;
+
+    for (int i = 0; i < arr.Length / 2; i++)
+    {
+        temp = arr[arr.Length - 1 - i];
+        arr[arr.Length - 1 - i] = arr[i];
+        arr[i] = temp;
+    }
+}
+
+
+int size = Prompt("Задай размер массива: ");
+
+int[] userArray = GetRandomArray(size);
+PrintArray(userArray);
+
+System.Console.WriteLine();
+
+ReverseArray(userArray);
+PrintArray(userArray);
