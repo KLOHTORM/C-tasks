b2b94df332327d890324b5cabc42d4be926c67a4
KLOHTORM <klohtorm@gmail.com>
2023-11-20 11:56:22 +0300

    Добавили таск 42

---
+++ task42/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+
+45 -> 101101
+3 -> 11
+2 -> 10
+*/
+
+System.Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int FindZeroOne(int number)
+{
+    int result = 0;
+    int tmp = 0;
+
+    for (int i = number; i > 0; i/=2)
+    {
+        tmp = i % 2;
+        result = tmp + result * 10;
+    }
+    return result;
+}
+
+System.Console.WriteLine(FindZeroOne(num));
