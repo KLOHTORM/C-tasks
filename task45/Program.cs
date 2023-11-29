481a8f4d6b7f039f2a88e03808a9be1da056891f
KLOHTORM <klohtorm@gmail.com>
2023-11-29 16:57:38 +0300

    Добавили таск 45

---
+++ task45/Program.cs
@@ -0,0 +1,49 @@
+﻿/*
+Напишите программу, которая будет создавать копию заданного одномерного массива с помощью поэлементного копирования.
+*/
+
+int[] GetNewArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(-99, 100);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write(array[i]);
+        if (i < array.Length)
+        {
+            System.Console.Write("  ");
+        }
+    }
+}
+
+int[] CloneArray(int[] arr)
+{
+    int size = arr.Length;
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = arr[i];
+    }
+    return array;
+}
+
+Console.Clear();
+
+System.Console.WriteLine("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int[] arr = GetNewArray(size);
+PrintArray(arr);
+
+System.Console.WriteLine();
+
+int[] array = CloneArray(arr);
+PrintArray(array);
