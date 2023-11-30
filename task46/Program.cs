ada712df59f4113714a874f9877e43939d483753
KLOHTORM <klohtorm@gmail.com>
2023-11-30 15:32:09 +0300

    Исправили таск 46

--- task46/Program.cs
+++ task46/Program.cs
@@ -10,7 +10,7 @@ int Massege(string massage)
     return result;
 }
 
-int[] GetRandom2DArray(int first, int last, int row, int column)
+int[,] GetRandom2DArray(int first, int last, int row, int column)
 {
     int[,] array = new int[row, column];
     for (int i = 0; i < row; i++)
@@ -23,18 +23,22 @@ int[] GetRandom2DArray(int first, int last, int row, int column)
     return array;
 }
 
-void Print2DArray(int[] array)
+void Print2DArray(int[,] array)
 {
-    for (int i = 0; i < array.Length; i++)
+    for (int i = 0; i < array.GetLength(0); i++)
     {
-        System.Console.Write(array[i]);
-        if (i < array.Length)
+        for (int j = 0; j < array.GetLength(1); j++)
         {
-            System.Console.Write(", ");
+        System.Console.Write(array[i, j] + "\t");
         }
+        System.Console.WriteLine();
     }
 
-int userArrX = Massege("Введите кол-во строк: ");
-int userArrY = Massege("Введите кол-во столбцов: ");
+int userArrRow = Massege("Введите кол-во строк: ");
+int userArrColumn = Massege("Введите кол-во столбцов: ");
 int userArrFirst = Massege("Введите начало диапазона: ");
 int userArrayLast = Massege("Введите конец диапазона: ");
+
+int[,] user2DArr = GetRandom2DArray( userArrFirst, userArrayLast, userArrRow, userArrColumn);
+
+Print2DArray(user2DArr);
