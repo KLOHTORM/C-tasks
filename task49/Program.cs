1afaf22414f22ef91c6788675de675139af70b50
KLOHTORM <klohtorm@gmail.com>
2023-12-01 11:24:28 +0300

    Добавили таск 49

---
+++ task49/Program.cs
@@ -0,0 +1,72 @@
+﻿/*
+Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, и замените эти элементы на их квадраты.
+*/
+
+Console.Clear();
+
+int Massege(string massage)
+{
+    System.Console.Write(massage);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+int[,] Get2DArr(int first, int last, int row, int column)
+{
+    int[,] arr = new int[row, column];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            arr[i, j] = new Random().Next(first, last+1);
+        }
+    }
+    return arr;
+}
+
+void Print2DArr(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            System.Console.Write(arr[i, j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int[,] ModifyIndex(int[,] arr)
+{
+    int[,] result = arr;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (i % 2 ==0 && j % 2 ==0)
+            {
+                result[i, j] = arr[i, j] * arr[i, j];
+            }
+        }
+    }
+    return result;
+}
+
+
+
+
+int userRow = Massege("Введите кол-во строк: ");
+int userColumn = Massege("Введите кол-во столбцов: ");
+int userFirst = Massege("Введите начало диапазона: ");
+int userLast = Massege("Введите конец диапазона: ");
+
+System.Console.WriteLine();
+
+int[,] user2DArr = Get2DArr(userFirst, userLast, userRow, userColumn);
+Print2DArr(user2DArr);
+
+System.Console.WriteLine();
+
+int[,] user2DArr1 = ModifyIndex(user2DArr);
+Print2DArr(user2DArr1);
