3444401128cb915e87feeffbdeaf1cd1cc883f1f
KLOHTORM <klohtorm@gmail.com>
2023-12-16 21:48:16 +0300

    Добавили таск 50

---
+++ task50/Program.cs
@@ -0,0 +1,74 @@
+﻿/*
+Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+Например, задан массив:
+1 4 4 2
+5 9 2 3
+8 4 2 4
+
+1 и 2 -> 2
+1 и 7 -> такого числа в массиве нет
+*/
+
+int Massege(string massage)
+{
+    System.Console.Write(massage);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+int[,] Get2DArr(int first, int last, int row, int column)
+{
+    int[,] arr = new int[row, column];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            arr[i, j] = new Random().Next(first, last+1);
+        }
+    }
+    return arr;
+}
+
+void Print2DArr(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            System.Console.Write(arr[i, j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int CheckNum(int[,] arr, int num)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        for (int j = 0; j < arr.Length; j++)
+        {
+            if (arr[i, j] == num) System.Console.WriteLine("нет");
+            else System.Console.WriteLine("есть");
+        }
+    }
+    return num;
+}
+
+
+
+
+int userRow = Massege("Введите кол-во строк: ");
+int userColumn = Massege("Введите кол-во столбцов: ");
+int userFirst = Massege("Введите начало диапазона: ");
+int userLast = Massege("Введите конец диапазона: ");
+int[,] user2DArr = Get2DArr(userFirst, userLast, userRow, userColumn);
+
+Print2DArr(user2DArr);
+System.Console.WriteLine();
+
+int number = Massege("Введите число для проверки: ");
+System.Console.WriteLine();
+
+System.Console.WriteLine(CheckNum(user2DArr, number));
