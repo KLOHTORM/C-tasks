74e9d800d09c4d2eff9c5b1df34991965cb5477c
KLOHTORM <klohtorm@gmail.com>
2023-12-19 11:33:33 +0300

    Добавили таск 67

---
+++ task67/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Выполнение через рекурсию (функция вызывает саму себя)
+
+Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+*/
+
+int Massege(string massage)
+{
+    System.Console.Write(massage);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+int NumSum(int num)
+{
+    if (num / 10 > 0)
+    {
+        return num % 10 + NumSum(num / 10);
+    }
+    return num;
+}
+
+int userNum = Massege("Введите число: ");
+System.Console.WriteLine(NumSum(userNum));
